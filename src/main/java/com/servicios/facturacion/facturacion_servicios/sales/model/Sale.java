b2c47e9ec0b07066925c57e40205252e20483ff7
David Ayme <dayme1937@uta.edu.ec>
2024-06-09 18:24:17 -0500

    Creacion de los modelos sales y sale details

---
+++ src/main/java/com/servicios/facturacion/facturacion_servicios/sales/model/Sale.java
@@ -0,0 +1,53 @@
+package com.servicios.facturacion.facturacion_servicios.sales.model;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.servicios.facturacion.facturacion_servicios.client.Client;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "sales")
+public class Sale {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id", nullable = false)
+    private Client client;
+
+    @Column(name = "date_sale", nullable = false)
+    private LocalDateTime dateSale;
+
+    @Column(nullable = false)
+    private float subtotal;
+
+    @Column(nullable = false)
+    private float total;
+
+    @Column(columnDefinition = "boolean default true")
+    @Builder.Default
+    private boolean status = true;
+
+    @OneToMany(mappedBy = "sales", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<SalesDetails> salesDetails;
+}
---
+++ src/main/java/com/servicios/facturacion/facturacion_servicios/sales/model/SalesDetails.java
@@ -0,0 +1,46 @@
+package com.servicios.facturacion.facturacion_servicios.sales.model;
+
+import com.servicios.facturacion.facturacion_servicios.product.Product;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "sales_details")
+public class SalesDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sale_id", nullable = false)
+    private Sale sales;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Column(nullable = false)
+    private float price;
+
+    @Column(nullable = false)
+    private float subtotal;
+}
