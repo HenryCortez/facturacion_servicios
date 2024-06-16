4216ac22358a8bdaba942b2be23e5529b5dfa78e
Henry Cortez <henrystevencortez@yahoo.es>
2024-06-15 20:58:01 -0500

    Se hizo commit con los json en el readme y se agrego decoradores de lombock en los dto de ventas

--- src/main/java/com/servicios/facturacion/facturacion_servicios/sales/dto/SaleRequestDTO.java
+++ src/main/java/com/servicios/facturacion/facturacion_servicios/sales/dto/SaleRequestDTO.java
@@ -1,9 +1,16 @@
 package com.servicios.facturacion.facturacion_servicios.sales.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SaleRequestDTO {
     private Long clientId;
     private List<SaleDetailDTO> saleDetails;
